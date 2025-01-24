9b2b5fa605391a201f9d41b0b7d68d3209d27957
Barat shankar <barathms96@gmail.com>
2025-01-24 20:11:37 +0530

    HeroSection

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,10 @@
 import NavBar from "./components/navbar/navbar"
-
+import HeroSection from "./components/HeroSection/HeroSection";
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <HeroSection/>
     </div>
   );
 }
---
+++ qtify/src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./HeroSection.module.css"
+import HeroImage from "../../Assets/vibrating-headphone 1.png"
+const HeroSection =()=>{
+return(
+    <div className={styles.HeroContainer}>
+        <div className={styles.HeroText}>
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+        </div>
+        <div><img className={styles.HeroImage} src={HeroImage} alt="HeadPhones"/></div>
+    </div>
+   )
+}
+
+export  default HeroSection;
